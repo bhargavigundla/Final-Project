39aa08e7d66b25fb72b6b11ffeb38f425344eda0
bgundla3 <bhargavigundla@gatech.edu>
2022-04-03 19:54:29 -0400

    imported lab 8 spritesheet to use as placeholder

---
+++ outsideCM.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(outsideCM)
+
+//======================================================================
+//
+//	outsideCM, 392x344@8, 
+//	+ bitmap not compressed
+//	Total size: 134848 = 134848
+//
+//	Time-stamp: 2022-04-03, 19:37:20
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_OUTSIDECM_H
+#define GRIT_OUTSIDECM_H
+
+#define outsideCMBitmapLen 134848
+extern const unsigned short outsideCMBitmap[67424];
+
+#endif // GRIT_OUTSIDECM_H
+
+//}}BLOCK(outsideCM)
