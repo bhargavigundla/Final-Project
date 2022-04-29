e811129df6c2a040206f9f3b0086aa717a78e35c
bgundla3 <bhargavigundla@gatech.edu>
2022-04-29 03:39:34 -0400

    removed art and tile files

--- tiles/forestForeground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="forestForeground" tilewidth="16" tileheight="16" tilecount="320" columns="32">
- <image source="../art/forestForegroundTiles.png" width="512" height="160"/>
-</tileset>
